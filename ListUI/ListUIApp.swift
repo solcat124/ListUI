37bec66387c0dfb613e35c5ddbeba7b138b5a2dd
solcat124 <pk80@windbow.com>
2025-03-06 08:33:40 -0800

    Initial Commit

---
+++ ListUI/ListUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListUIApp.swift
+//  ListUI
+//
+//  Created by Phil Kelly on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ListUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ListUITests/ListUITests.swift
@@ -0,0 +1,17 @@
+//
+//  ListUITests.swift
+//  ListUITests
+//
+//  Created by Phil Kelly on 3/6/25.
+//
+
+import Testing
+@testable import ListUI
+
+struct ListUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
